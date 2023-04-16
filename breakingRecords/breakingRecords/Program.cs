06b04080e0b3bcdfce8a8f37f06f0ea765b09b83
gonkov <gonkov@gmail.com>
2023-04-16 20:49:52 +0300

    create project 'breakingRecords'

---
+++ breakingRecords/breakingRecords/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace breakingRecords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
