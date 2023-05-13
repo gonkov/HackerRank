203e86193bdfd8611d135747b5329ed89d0118fc
gonkov <gonkov@gmail.com>
2023-05-13 17:12:37 +0300

    equalizeArray DONE

---
+++ Equalize the Array/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'equalizeArray' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int equalizeArray(List<int> arr)
+    {
+        List<int> dict = new List<int>(101);
+        for (int i = 0; i < 101; i++)
+        {
+            dict.Add(0);
+        }
+        foreach (int item in arr)
+        {
+            dict[item]++;
+        }
+        int max = 0;
+        foreach (int item in dict)
+        {
+            if (item > max)
+            {
+                max = item;
+            }
+        }
+        return arr.Count - max;
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        int result = Result.equalizeArray(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
